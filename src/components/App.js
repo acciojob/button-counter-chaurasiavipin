a4dd9c5d6e6d889e00abebb66209c53947a84675
chaurasiavipin <vipinchaurasia481@gmail.com>
2026-01-04 14:53:17 +0530

    Refactor App component to remove duplicate code
    Removed duplicate state and click handler definitions from the App component.

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,15 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [value,setCount]=useState(0);
+    const onClick=()=>{
+      setCount(value+1);
+    }
   return (
     <div>
         {/* Do not remove the main div */}
-    const [value,setCount]=useState(0)
-    const onClick =()=>{
-      setCount(value+1);
-    }
+    
+    
     <p>Button clicked {value} times</p>
     <button onclick={onClick}>Click me</button>
     </div>
