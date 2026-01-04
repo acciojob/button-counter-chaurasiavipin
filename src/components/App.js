b708182ad9ce51c456e7b4f259c3df130e2dbc8c
chaurasiavipin <vipinchaurasia481@gmail.com>
2026-01-04 15:00:24 +0530

    Fix variable name and button click handler

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [value,setCount]=useState(0);
+  const [count,setCount]=useState(0);
     // const onClick=()=>{
     //   setCount(value+1);
     // }
@@ -13,8 +13,8 @@ const App = () => {
         {/* Do not remove the main div */}
     
     
-    <p>Button clicked {value} times</p>
-    <button onclick={onClick=()=>setCount(value+1)}>Click me</button>
+    <p>Button clicked {count} times</p>
+    <button oonClick={()=>setCount(count+1)}>Click me</button>
     </div>
   )
 }
