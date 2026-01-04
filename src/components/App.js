b09ece07ad685b150659cbf292e289cfd393a149
chaurasiavipin <vipinchaurasia481@gmail.com>
2026-01-04 15:05:30 +0530

    Fix count update in App component

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import './../styles/App.css';
 const App = () => {
   const [count,setCount]=useState(0);
     const Onclick=()=>{
-      setCount(value+1);
+      setCount(count+1);
     }
   return (
     <div>
