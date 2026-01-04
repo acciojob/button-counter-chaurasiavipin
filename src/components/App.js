4d83318b81385ac4b287796d911ab29aa2755b59
chaurasiavipin <vipinchaurasia481@gmail.com>
2026-01-04 15:02:20 +0530

    Fix button click handler and state update logic

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,16 @@ import './../styles/App.css';
 
 const App = () => {
   const [count,setCount]=useState(0);
-    // const onClick=()=>{
-    //   setCount(value+1);
-    // }
+    const Onclick=()=>{
+      setCount(value+1);
+    }
   return (
     <div>
         {/* Do not remove the main div */}
     
     
     <p>Button clicked {count} times</p>
-    <button oonClick={()=>setCount(count+1)}>Click me</button>
+    <button onClick={Onclick}>Click me</button>
     </div>
   )
 }
