35231e0c43d452e33eef18cf073f8da7f6ea6e53
chaurasiavipin <vipinchaurasia481@gmail.com>
2026-01-05 22:22:52 +0530

    Fix typo in import statement for useState

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import './../styles/App.css';
-import {useSate} from "react"
+import {useState} from "react"
 
 const App = () => {
   const [count,setCount]=useState(0);
